085df6f2364eeab0fd61d91007abf2df7921a6a7
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-01-13 09:16:54 -0300

    alteração no arquivo template read.php

---
+++ read.php
@@ -0,0 +1,8 @@
+<?php
+include 'functions.php';
+// Connect to MySQL database
+$pdo = pdo_connect_mysql();
+// Get the page via GET request (URL param: page), if non exists default the page to 1
+$page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
+// Number of records to show on each page
+$records_per_page = 5;
